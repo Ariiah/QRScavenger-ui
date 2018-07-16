a35024e6bc8485575724ab7734d403452d2f32f7
Kelly L <lock2846@gmail.com>
2018-07-16 14:29:40 -0600

    project created

---
+++ QRScavanger/QRScavanger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QRScavanger
+//
+//  Created by Aria on 7/16/18.
+//  Copyright © 2018 Kelly Lockwood. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
