4036fff21dd377d1364e6a438e10aa4749f356bb
Kelly L <lock2846@gmail.com>
2018-07-19 14:27:05 -0600

    text appears on note pad

--- QRScavanger/QRScavanger/FirstViewController.swift
+++ QRScavanger/QRScavanger/FirstViewController.swift
@@ -2,20 +2,37 @@
 //  ViewController.swift
 //  AR Test
 //
-//  Created by Aria on 7/18/18.
+//  Created by Kelly Lockwood on 7/18/18.
 //  Copyright © 2018 Kelly Lockwood. All rights reserved.
 //
 
 import UIKit
 import SceneKit
 import ARKit
+//import Alamofire
+//import SwiftyJSON
 
 class FirstViewController: UIViewController, ARSCNViewDelegate {
     
-    //    @IBOutlet var sceneView: ARSCNView!
-    
-    
     @IBOutlet weak var sceneView: ARSCNView!
+    
+    // Show text
+    func displayText() {
+        
+        let textGeo = SCNText(string: "Hello", extrusionDepth: 1.0)
+        
+        textGeo.firstMaterial?.diffuse.contents = UIColor.black
+        textGeo.font = UIFont(name: "Arial", size: 2)
+        
+        let textNode = SCNNode(geometry: textGeo)
+        textNode.position = SCNVector3(-0.05,0.15,-0.29)
+        textNode.scale = SCNVector3(0.002,0.002,0.002)
+        
+        self.sceneView.scene.rootNode.addChildNode(textNode)
+        
+//        let action = SCNAction.fadeOpacity(by: -1, duration: 5.0)
+//        textNode.runAction(action)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +40,15 @@ class FirstViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-//        let material = SCNMaterial()
-        
-//        material.diffuse.contents = UIImage(named: "art.scnassets/note_pad_large.scn")
-        
-        // gives it light and shadows to appear 3D
-        
-//        sceneView.autoenablesDefaultLighting = true
-        
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/Note pad final.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        // Render text on screen
+        displayText()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
